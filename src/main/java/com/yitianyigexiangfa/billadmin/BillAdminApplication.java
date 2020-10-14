4e5987113ed9c97d51d95430b11d0b1892d5e070
刘备 <admin@yitianyigexiangfa.com>
2020-10-14 18:18:10 +0800

    first commit

---
+++ src/main/java/com/yitianyigexiangfa/billadmin/BillAdminApplication.java
@@ -0,0 +1,13 @@
+package com.yitianyigexiangfa.billadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yitianyigexiangfa/billadmin/BillAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yitianyigexiangfa.billadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
